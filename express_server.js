356af9f412682844bc830a5d4229004e894b4fdf
JasTano <jasmine.broodhagen@outlook.com>
2025-06-22 20:04:52 -0400

    Add new URL form and POST route

--- express_server.js
+++ express_server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,13 +25,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id]; // get long URL for given short ID
   const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`tinyapp listening on port ${PORT}!`);
@@ -35,5 +47,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // logs e.g. { longURL: 'http://example.com' }
+  res.send("Ok");
 });
 
