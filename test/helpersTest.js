e2c3615691e7d1929c7f2617a3e05081baf3c6a6
JasTano <jasmine.broodhagen@outlook.com>
2025-06-29 23:42:41 -0400

    Add tests for urlsForUser helper function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers');
+const { getUserByEmail, urlsForUser } = require('../helpers');
 
 const testUsers = {
   "userRandomID": {
@@ -24,5 +24,40 @@ describe('getUserByEmail', function() {
   it('should return null for an email not in database', function() {
     const user = getUserByEmail("nonexistent@example.com", testUsers);
     assert.isNull(user);
+  });
+});
+
+describe('urlsForUser', function() {
+  const urlDatabase = {
+    "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "user1" },
+    "9sm5xK": { longURL: "http://www.google.com", userID: "user2" },
+    "a1b2c3": { longURL: "http://www.example.com", userID: "user1" }
+  };
+
+  it('should return urls that belong to the specified user', function() {
+    const result = urlsForUser("user1", urlDatabase);
+    const expected = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "user1" },
+      "a1b2c3": { longURL: "http://www.example.com", userID: "user1" }
+    };
+    assert.deepEqual(result, expected);
+  });
+
+  it('should return an empty object if the user has no urls', function() {
+    const result = urlsForUser("user3", urlDatabase);
+    assert.deepEqual(result, {});
+  });
+
+  it('should return an empty object if the urlDatabase is empty', function() {
+    const result = urlsForUser("user1", {});
+    assert.deepEqual(result, {});
+  });
+
+  it('should not return urls that belong to other users', function() {
+    const result = urlsForUser("user2", urlDatabase);
+    const expected = {
+      "9sm5xK": { longURL: "http://www.google.com", userID: "user2" }
+    };
+    assert.deepEqual(result, expected);
   });
 });
